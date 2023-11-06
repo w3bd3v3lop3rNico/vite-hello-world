fab925559a67acf263d855894cdf7df7fbd33738
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-06 09:28:26 +0100

    add title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  data() {
+    return {
+      title: 'La mia prima app con Vite!'
+    }
+  },
+}
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,11 +18,13 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
+<h1> {{ title }}</h1>
+
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -26,5 +35,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
